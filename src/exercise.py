fefa6493e456491e9bb0b51ab44e28fd11c04995
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:27:04 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    num1 = int(input('Give the first number:'))
+    num2 = int(input('Give the second number:'))
+    
+    if num1 > num2:
+        print(str(num1) + ' is bigger than ' + str(num2))
+    elif num1 < num2:
+        print(str(num2) + ' is bigger than ' + str(num1))
+    else:
+        print('The numbers are equal!')
 
 if __name__ == '__main__':
     main()
